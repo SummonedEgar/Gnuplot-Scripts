be9db07f94c0b7a22cb7004e4f0809aa81dae77e
SummonedEgar <aldast10@yahoo.it>
2016-12-09 17:56:43 +0100

    1.0

---
+++ Circuits/Z80 II/Z80_CALIBRAZIONE/Z80_CALIBRAZIONE.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+
+gnuplot <<EOF
+
+#terminal type
+set terminal pngcairo size 1600, 900 enhanced font 'Verdana,15'
+set output 'nice_web_plot.png'
+
+#title
+set title "Retta di Calibrazione"
+
+#background and axis
+set style line 11 lc rgb '#282828' lt 1
+set border 3 back ls 11
+set tics nomirror
+
+set style line 12 lc rgb '#282828' lt 0 lw 2
+set grid back ls 12
+
+#line styles
+set style line 1 lc rgb '#5e9c36' pt 6 ps 2 lt 1 lw 3 # --- green
+set style line 2 lc rgb '#660000' pt 6 ps 2 lt 1 lw 3 # --- red
+set style line 3 lc rgb '#0022ee' pt 6 ps 2 lt 1 lw 3 # --- blue
+set style line 4 lc rgb '#764c64' pt 6 ps 2 lt 1 lw 3 # --- pink
+set style line 5 lc rgb '#1155ff' pt 6 ps 2 lt 1 lw 3 # --- blueish
+
+#key
+set key top right
+
+#x axis
+#set log x
+set format x "%x"
+set xtics font ", 14"
+
+#labels
+set xlabel 'Step'
+set ylabel 'Vout (V)'
+#set xrange [0:20]
+set yrange [0:6]
+
+#fit
+f(x) = a*x +b
+fit f(x) 'retta calibr' u 1:2 via a,b 
+
+#plot
+plot 'retta calibr' u 1:2:(\$2*0.00001+0.18) w yerrorbars ls 2 t 'data', \\
+	a*x+b ls 1 t 'fit'
+
+EOF
+
